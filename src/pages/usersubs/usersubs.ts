1d36a7605dcea905933883788d74b750e014d706
Linds Gray <lindsaygray11@gmail.com>
2017-12-13 13:04:56 -0800

    feat(org subs): changed subs page depending on user

--- src/pages/usersubs/usersubs.ts
+++ src/pages/usersubs/usersubs.ts
@@ -20,27 +20,43 @@ export class UsersubsPage {
   headers: Headers;
   allOrganizations: any = [];
   title: string = this.navParams.get('title');
+  user: any;
+  org: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http) {
     this.headers = new Headers();
     this.headers.append('Content-Type', 'application/json');
+    if (JSON.parse(localStorage.getItem('user')).user) {
+      this.user = JSON.parse(localStorage.getItem('user')).user;
+    } else {
+      this.org = JSON.parse(localStorage.getItem('user')).org
+    }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UsersubsPage');
-    this.http.get('http://localhost:8080/organization/organization')
-      .toPromise()
-      .then(response => {
-        if(response.json().org) {
-          this.allOrganizations = response.json().org;
-          this.allOrganizations.forEach(org => {
-            org.categories = org.categories.filter(category => {
-              return category.categoryName === this.navParams.get('filter');
+    if(this.user) {
+      this.http.get('http://localhost:8080/organization/organization')
+        .toPromise()
+        .then(response => {
+          if (response.json().org) {
+            this.allOrganizations = response.json().org;
+            this.allOrganizations.forEach(org => {
+              org.categories = org.categories.filter(category => {
+                if (category.categoryName) {
+                  return category.categoryName === this.navParams.get('filter')[0] || category.categoryName === this.navParams.get('filter')[1];
+                } else return false;
+              });
             });
-            console.log(org.categories);
-          });
-        }
+          }
+        });
+    } else {
+      this.org.categories = this.org.categories.filter(category => {
+        if (category.categoryName) {
+          return category.categoryName === this.navParams.get('filter')[0] || category.categoryName === this.navParams.get('filter')[1];
+        } else return false;
       });
+    }
   }
 
 }
